2b8ffb6bd572e88fbbbe68cd55f2f4f06866f17a
Sam Gross <colesbury@gmail.com>
2013-09-04 20:39:48 -0400

    smoothstep(0.0, 0.0, x) returns 0.0 on Android and 1.0 on iOS.

--- cocos2dx/shaders/ccShader_PositionColorLengthTexture_frag.h
+++ cocos2dx/shaders/ccShader_PositionColorLengthTexture_frag.h
@@ -33,7 +33,10 @@ varying vec2 v_texcoord;																									\n\
 void main()																													\n\
 {																															\n\
  #if defined GL_OES_standard_derivatives																						\n\
-	 gl_FragColor = v_color*smoothstep(0.0, length(fwidth(v_texcoord)), 1.0 - length(v_texcoord));							\n\
+	 if (length(fwidth(v_texcoord)) > 0.0)																					\n\
+		 gl_FragColor = v_color*smoothstep(0.0, length(fwidth(v_texcoord)), 1.0 - length(v_texcoord));						\n\
+	 else																													\n\
+		 gl_FragColor = v_color;																							\n\
  #else																														\n\
 	gl_FragColor = v_color*step(0.0, 1.0 - length(v_texcoord));																\n\
  #endif																														\n\
